f613b1bd1a8dff00dd8ad986a75a9bc1e6e4bc34
Travis Parks <travisparks0788@gmail.com>
2022-12-01 20:23:18 -0500

    Add complete all tests

--- src/book.js
+++ src/book.js
@@ -2,19 +2,45 @@ function createTitle(title) {
   return  `The ${title}`
 }
 
-function buildMainCharacter(name, age, pronouns) {
 
-return 
+
+function buildMainCharacter(name, age, pronouns) {
+    return {
+      name: name,
+      age: age,
+      pronouns: pronouns
+    }
 }
 
+function saveReview(newReview, existingReview) {
+ if (existingReview.indexOf(newReview) === -1) {
+  existingReview.push(newReview)
+ }
+}
 
+function calculatePageCount(title) {
+  return title.length * 20
+}
 
+function writeBook(title, mainCharacter, genre) {
+  return {
+    title: title,
+    mainCharacter: mainCharacter,
+    pageCount: calculatePageCount(title),
+    genre: genre,
+    
+  }
+}
+
+function editBook(title){
+  return title.pageCount *= .75
+}
 
 module.exports = {
   createTitle,
    buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+   saveReview,
+   calculatePageCount,
+    writeBook,
+   editBook
 }
