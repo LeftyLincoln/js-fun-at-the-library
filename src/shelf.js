8dbc0025cd68b1bce751a2b1ebde9093d73e2b1e
Travis Parks <travisparks0788@gmail.com>
2022-12-02 20:07:06 -0500

    Add answers for unshelf book

--- src/shelf.js
+++ src/shelf.js
@@ -1,11 +1,31 @@
 
-module.exports = {
-  shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
-};
 
 function shelfBook(object, shelf) {
-   return shelf.unshift(object)
-}  
+  if (shelf.length < 3)
+  shelf.unshift(object)
+ } 
+ 
+
+function unshelfBook(object, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+  if (shelf[i].title === object) {
+    shelf.splice(i, 1)
+  }
+}
+}
+
+function listTitles() {
+  
+}
+
+
+
+
+
+
+module.exports = {
+  shelfBook,
+   unshelfBook,
+  listTitles,
+  // searchShelf
+};
