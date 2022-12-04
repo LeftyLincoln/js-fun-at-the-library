2a68347454b36ec9b620bd25a1e717e2db14f1a0
Travis Parks <travisparks0788@gmail.com>
2022-12-03 19:19:26 -0500

    Add library answers

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,37 @@
+function createLibrary(name, shelves) {
+    return {
+      name: name,
+      shelves: {
+        fantasy: [],
+        fiction: [],
+        nonFiction: []
+      } 
+    }
+}
+
+function addBook(name, object) {
+    name.shelves[object.genre].push(object)
+}
+
+
+function checkoutBook(library, book, shelfGenre) {
+  for (var i = 0; i < library["shelves"][shelfGenre].length; i++) {
+    if (library["shelves"][shelfGenre][i].title === book) {
+      library["shelves"][shelfGenre].splice(i, 1)
+      return `You have now checked out ${book} from the ${library.name}`
+    } 
+  }
+  return `Sorry, there are currently no copies of ${book} available at the ${library.name}`
+}
+
+
+
+
+
 
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+   createLibrary,
+   addBook,
+  checkoutBook
 };
